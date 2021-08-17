1febb68fea1077a3470ca06ec0e7081683a2aeb4
neha-11coder <83291551+neha-11coder@users.noreply.github.com>
2021-08-17 12:26:24 +0530

    Add files via upload

---
+++ paperball.js
@@ -0,0 +1,24 @@
+class Paperball{
+
+    constructor(){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:1.2,
+            density:1.2
+    }
+    this.body=Bodies.circle(100,100,75,options);
+    this.radius=75;
+   
+    this.image=loadImage("images/ball.png");
+
+    World.add(world,this.body)
+    }
+    display(){
+        push();
+        translate (this.body.position.x,this.body.position.y);
+        imageMode(CENTER);
+        image(this.image,0,0,this.radius,this.radius);
+        pop ();
+    }
+        }
